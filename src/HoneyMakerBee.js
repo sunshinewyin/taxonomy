765921e5f126ed58a878dac7a433f1c00a591c25
sunshinewyin <sunshine.w.yin@gmail.com>
2015-03-30 15:02:11 -0700

    Completed all taxonomy tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
